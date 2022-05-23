9adc76f529d0aaf899ef9a74c2b60e507d00839d
Frederico B. K <frekle@kth.se>
2022-05-23 18:00:13 +0200

    missing the beginning though

--- src/grf_mot_file_dumper.cpp
+++ src/grf_mot_file_dumper.cpp
@@ -1,12 +1,20 @@
+#include "InverseDynamics.h"
 #include "OpenSimUtils.h"
 #include "SignalProcessing.h"
 #include <Actuators/Thelen2003Muscle.h>
 #include <Common/TimeSeriesTable.h>
 #include <OpenSim/Common/STOFileAdapter.h>
+#include "geometry_msgs/Vector3.h"
+#include "ros/init.h"
+#include "ros/message_traits.h"
+#include "ros/node_handle.h"
+#include "ros/publisher.h"
 #include "ros/ros.h"
+#include "opensimrt_msgs/PointWrenchTimed.h"
 #include "opensimrt_msgs/CommonTimed.h"
-#include "opensimrt_msgs/Labels.h"
+#include "ros/subscriber.h"
 #include "std_msgs/Header.h"
+#include <SimTKcommon/SmallMatrix.h>
 #include <cstdlib>
 
 using namespace std;
@@ -14,116 +22,176 @@ using namespace OpenSim;
 using namespace SimTK;
 using namespace OpenSimRT;
 
-void run() {
-    // subject data
-    //auto section = "TEST_ACCELERATION_GRFM_PREDICTION_FROM_FILE";
-    ros::NodeHandle nh("~");
-    string subject_dir, model_file, ik_file;
-    nh.param<std::string>("model_file", model_file, "");
-    nh.param<std::string>("ik_file", ik_file, "");
-    
-    // repeat cyclic motion X times
-    int simulation_loops;
-    nh.param<int>("simulation_loops", simulation_loops, 0);
-    // remove last N samples in motion for smooth transition between loops
-    int remove_n_last_rows;
-    nh.param<int>("remove_n_last_rows", remove_n_last_rows, 0);
-
-    //cheat
-    double resample_period;
-    nh.param<double>("resample_period", resample_period, 0.01);
-    // setup model
-    Object::RegisterType(Thelen2003Muscle());
-    Model model(model_file);
-    model.initSystem();
-
-    // get kinematics as a table with ordered coordinates
-    auto qTable = OpenSimUtils::getMultibodyTreeOrderedCoordinatesFromStorage(
-            model, ik_file, resample_period); ///this is resampling!!!!!
-    ROS_WARN_STREAM("The precision of this algorithm and data downstream is inflated. IK data was resampled! ");
-
-    ROS_INFO_STREAM("Number of variables measured: " << qTable.getRowAtIndex(0).size());
-    // remove last rows in qTable
-    for (int i = 0; i < remove_n_last_rows; ++i)
-        qTable.removeRow(qTable.getIndependentColumn().back());
-
-    ros::NodeHandle n;
-    ros::Publisher re_pub = n.advertise<opensimrt_msgs::CommonTimed>("r_data", 1000);
-    ros::Publisher labels_pub = n.advertise<opensimrt_msgs::Labels>("r_labels", 1000, true); //latching topic
-
-    std::vector<std::string> labels = qTable.getColumnLabels();
-    opensimrt_msgs::Labels l_msg;
-    l_msg.data = labels;
-    std_msgs::Header h;
-    h.stamp = ros::Time::now();
-    h.frame_id = "subject";
-    l_msg.header = h;	
-    labels_pub.publish(l_msg); 
-    //ros::spinOnce();
-    
-    ros::Rate rate(1/resample_period);
-    // repeat the simulation `simulationLoops` times
-    int simulationLimit = qTable.getNumRows() * simulation_loops;
-
-    
-    ROS_INFO_STREAM("Simulation limit:" << simulationLimit );
-
-    double time_error_threshold = 10*resample_period; // in seconds
-    double initial_time = ros::Time::now().toSec();
-
-    for (int j = 0; j < simulation_loops; j++) {
-	    double time_offset = j * (qTable.getIndependentColumn().back()+resample_period); // we need resample period or 
-	    ROS_INFO_STREAM("Time offset:" << time_offset );
-	    for (int i = 0; i < qTable.getNumRows(); i++) {
-		// get raw pose from table
-		ROS_DEBUG("Get raw pose from table");
-		auto qqqqq = qTable.getRowAtIndex(i);
-		opensimrt_msgs::CommonTimed msg;
-		std_msgs::Header h;
-		
-		double t = qTable.getIndependentColumn()[i] + time_offset;
-			
-		//msg.data.push_back(t);
-		msg.time = t;
-
-		for (auto ele: qqqqq)
-		{
-			ROS_DEBUG_STREAM("Element: " << ele);
-			msg.data.push_back(ele);
-		}
-		ROS_DEBUG("finished reading table row into msg.");
-
-
-    		//labels_pub.publish(l_msg); 
-
-
-		//auto qRaw = qTable.getRowAtIndex(i).getAsVector();
-		//ROS_DEBUG("DD");
-
-		h.frame_id = "subject";
-		ros::Time frameTime = ros::Time::now();
-		double time_difference = frameTime.toSec() - initial_time;
-		if ( std::abs(time_difference - t) > time_error_threshold )
-		{
-			
-			ROS_WARN_STREAM("time difference" << time_difference -t << " exceeds threshold: " << time_error_threshold);
-			ROS_INFO_STREAM("Frame time - initial_time " << time_difference << " Simulation time" << t );
-		}
-		h.stamp = frameTime;
-		msg.header = h;
-		re_pub.publish(msg);
-
-		rate.sleep();
-	    }
-    }
-
+//! oh, yeah, this guy.
+template <class T>
+T get_as(const SimTK::Vec3& v)
+{
+	//geometry_msgs::Vector3 outvec;
+	T outvec;
+	outvec.x = v[0];
+	outvec.y = v[1];
+	outvec.z = v[2];
+	return outvec;
 }
 
+
+class Gfrm
+{
+	//some important variables should be here
+	LowPassSmoothFilter* grfRightFilter, *grfLeftFilter;
+    	Storage* grfMotion;
+        bool use_filter;
+	vector<string> grfRightLabels, grfLeftLabels; 
+//publishers
+	ros::Publisher r_pub, l_pub;	
+	public:
+		Gfrm()
+		{
+    // subject data
+    //auto section = "TEST_ID_FROM_FILE";
+    ros::NodeHandle nh("~");
+    string grf_mot_file;
+    nh.param<std::string>("grf_mot_file", grf_mot_file, "");
+    
+    //GFRM params
+    string grfRightApplyBody;
+    nh.param<string>("grf_right_apply_to_body", grfRightApplyBody, "");
+    string grfRightForceExpressed;
+    nh.param<string>("grf_right_force_expressed_in_body", grfRightForceExpressed, "");
+    string grfRightPointExpressed;
+    nh.param<string>("grf_right_point_expressed_in_body", grfRightPointExpressed, "");
+    string grfRightPointIdentifier;
+    nh.param<string>("grf_right_point_identifier", grfRightPointIdentifier, "");
+    string grfRightForceIdentifier;
+    nh.param<string>("grf_right_force_identifier", grfRightForceIdentifier, "");
+    string grfRightTorqueIdentifier;
+    nh.param<string>("grf_right_torque_identifier", grfRightTorqueIdentifier, "");
+
+    string grfLeftApplyBody;
+    nh.param<string>("grf_left_apply_to_body", grfLeftApplyBody, "");
+    string grfLeftForceExpressed;
+    nh.param<string>("grf_left_force_expressed_in_body", grfLeftForceExpressed, "");
+    string grfLeftPointExpressed;
+    nh.param<string>("grf_left_point_expressed_in_body", grfLeftPointExpressed, "");
+    string grfLeftPointIdentifier;
+    nh.param<string>("grf_left_point_identifier", grfLeftPointIdentifier, "");
+    string grfLeftForceIdentifier;
+    nh.param<string>("grf_left_force_identifier", grfLeftForceIdentifier, "");
+    string grfLeftTorqueIdentifier;
+    nh.param<string>("grf_left_torque_identifier", grfLeftTorqueIdentifier, "");
+
+    //Params for GFRM filter
+    nh.param<bool>("use_filter", use_filter, true);
+    int memory;
+    nh.param<int>("memory", memory, 0);
+    double cutoffFreq; 
+    nh.param<double>("cutoff_freq", cutoffFreq, 0);
+    int delay;
+    nh.param<int>("delay", delay, 0);
+    int splineOrder;
+    nh.param<int>("spline_order", splineOrder, 0);
+
+    // setup external forces
+    grfMotion = new Storage(grf_mot_file);
+    
+    ExternalWrench::Parameters grfRightFootPar{
+            grfRightApplyBody, grfRightForceExpressed, grfRightPointExpressed};
+    grfRightLabels = ExternalWrench::createGRFLabelsFromIdentifiers(
+            grfRightPointIdentifier, grfRightForceIdentifier,
+            grfRightTorqueIdentifier);
+    auto grfRightLogger = ExternalWrench::initializeLogger();
+
+    ExternalWrench::Parameters grfLeftFootPar{
+            grfLeftApplyBody, grfLeftForceExpressed, grfLeftPointExpressed};
+    grfLeftLabels = ExternalWrench::createGRFLabelsFromIdentifiers(
+            grfLeftPointIdentifier, grfLeftForceIdentifier,
+            grfLeftTorqueIdentifier);
+    auto grfLeftLogger = ExternalWrench::initializeLogger();
+
+    vector<ExternalWrench::Parameters> wrenchParameters;
+    wrenchParameters.push_back(grfRightFootPar);
+    wrenchParameters.push_back(grfLeftFootPar);
+
+    LowPassSmoothFilter::Parameters grfFilterParam;
+    grfFilterParam.numSignals = 9;
+    grfFilterParam.memory = memory;
+    grfFilterParam.delay = delay;
+    grfFilterParam.cutoffFrequency = cutoffFreq;
+    grfFilterParam.splineOrder = splineOrder;
+    grfFilterParam.calculateDerivatives = false;
+    grfRightFilter = new LowPassSmoothFilter (grfFilterParam);
+    grfLeftFilter = new LowPassSmoothFilter (grfFilterParam);
+    
+    ros::NodeHandle n;
+    l_pub = n.advertise<opensimrt_msgs::PointWrenchTimed>("grf_left", 1000);
+    r_pub = n.advertise<opensimrt_msgs::PointWrenchTimed>("grf_right", 1000);
+    
+    double initial_time = ros::Time::now().toSec();
+       
+		}
+void pub_wrench(const ros::Publisher& pub, const ExternalWrench::Input& wrench, double t, const std_msgs::Header h)
+{
+		//double t = qTable.getIndependentColumn()[i] + time_offset;
+	        opensimrt_msgs::PointWrenchTimed msg;	
+		
+		msg.wrench.force = get_as<geometry_msgs::Vector3>(wrench.force);
+		msg.wrench.torque = get_as<geometry_msgs::Vector3>(wrench.torque);
+		msg.point = get_as<geometry_msgs::Point>(wrench.point);
+
+		msg.time = t;
+
+		ROS_DEBUG("created PointWrenchTimed msg ok.");
+		msg.header = h;
+		pub.publish(msg);
+}
+
+void pub_both_wrenches(double t)
+{
+    // receives time:
+    // get grf forces
+    	ROS_INFO_STREAM("T" << t);
+        auto grfRightWrench = ExternalWrench::getWrenchFromStorage(
+                t, grfRightLabels, *grfMotion);
+        auto grfLeftWrench = ExternalWrench::getWrenchFromStorage(
+                t, grfLeftLabels, *grfMotion);
+
+	//not sure why we need the filtered version since we are not really using it. 
+        if (use_filter) {
+		auto grfRightFiltered =
+                	grfRightFilter->filter({t, grfRightWrench.toVector()});
+        	grfRightWrench.fromVector(grfRightFiltered.x);
+        	auto grfLeftFiltered =
+                	grfLeftFilter->filter({t, grfLeftWrench.toVector()});
+        	grfLeftWrench.fromVector(grfLeftFiltered.x);
+        	if (!grfRightFiltered.isValid || !grfLeftFiltered.isValid) {
+            		return;
+        	}
+	}
+
+	std_msgs::Header h;
+	h.frame_id = "subject";
+	ros::Time frameTime = ros::Time::now();
+	h.stamp = frameTime;
+
+	pub_wrench(r_pub, grfRightWrench,t, h);
+	pub_wrench(l_pub, grfLeftWrench,t, h);
+
+}
+void operator() (const opensimrt_msgs::CommonTimedConstPtr& message){
+
+    //LOOP needs to be implemented like functor
+    double t = message->time;
+    pub_both_wrenches(t);
+}
+};
 int main(int argc, char** argv) {
     try { //should be avoided.
-	ros::init(argc, argv, "ik_file_bridge");
-
-        run();
+	ros::init(argc, argv, "grf_mot_file_bridge");
+	ros::NodeHandle n;
+	ROS_INFO_STREAM("Starting GRFM node");
+	//this is completely wrong. I should be able to just play it. but then how do I know that these things are synchronized?
+	ros::Subscriber sub = n.subscribe<opensimrt_msgs::CommonTimed>("r_data", 1, Gfrm());
+	ros::spin();
     } 
     catch ( ros::Exception &e ) {
         ROS_ERROR("ROS Error occured: %s ", e.what());
